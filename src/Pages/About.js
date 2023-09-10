fc0f1a4a3e325f4a016ff3fb05d254a635d999d4
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-09-10 22:44:27 +0530

    style

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function About() {
   // State to store the selected content
@@ -37,39 +35,48 @@ function About() {
     <>
       <Navbar />
       <div className="container mt-5">
-        <h1>About Us</h1>
-        <p>Welcome to our About Us page!</p>
+        <h1 className="text-center">About Us</h1>
+        <p className="text-center">Welcome to our About Us page!</p>
 
-        <div className="dropdown mt-4">
-          <button
-            className="btn btn-primary dropdown-toggle"
-            type="button"
-            id="aboutDropdown"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Select an option
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
-            {dropdownItems.map((item, index) => (
-              <li key={index}>
-                <a
-                  className="dropdown-item"
-                  href="#"
-                  onClick={() => handleDropdownSelection(item.content)}
-                >
-                  {item.title}
-                </a>
-              </li>
-            ))}
-          </ul>
+        <div className="row justify-content-center mt-4">
+          <div className="col-md-6">
+            <div className="dropdown">
+              <button
+                className="btn btn-primary btn-block dropdown-toggle"
+                type="button"
+                id="aboutDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Select an option
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                {dropdownItems.map((item, index) => (
+                  <li key={index}>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => handleDropdownSelection(item.content)}
+                    >
+                      {item.title}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
 
         {/* Display selected content */}
         {selectedContent && (
-          <div className="mt-4">
-            <h2>Selected Content</h2>
-            <p>{selectedContent}</p>
+          <div className="row justify-content-center mt-4">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="card-title">Selected Content</h2>
+                  <p className="card-text">{selectedContent}</p>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
