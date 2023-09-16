025b065f5783976322b4ecf6595e4fd86450eb2b
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-09-16 22:34:23 +0530

    .

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,5 @@
 import React, { useState} from 'react';
 import Navbar from '../components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function About() {
